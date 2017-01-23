39a95b85beb5e7d1490a1254501d753bf714a66c
JongHo.kim <pia-a@hanmail.net>
2017-01-23 13:13:05 +0900

    imageView 연습

--- app/src/main/java/com/example/user/aclass/MainActivity.java
+++ app/src/main/java/com/example/user/aclass/MainActivity.java
@@ -3,22 +3,13 @@ package com.example.user.aclass;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.btn);
-        button.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(this, "현석이형 왜 안해요?", Toast.LENGTH_SHORT).show();
     }
 }
