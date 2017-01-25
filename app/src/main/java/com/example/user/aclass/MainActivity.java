308144d53561ead1264abcb127d2e605504c602c
JongHo.kim <pia-a@hanmail.net>
2017-01-25 13:09:44 +0900

    커피 앱 완성본

--- app/src/main/java/com/example/user/aclass/MainActivity.java
+++ app/src/main/java/com/example/user/aclass/MainActivity.java
@@ -13,12 +13,19 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final int QUANTITY_MIN = 0;
+    public static final int QUANTITY_MAX = 10;
+    public static final int COFFEE_PRICE = 3000;
+
     private Button mMinusButton;
     private Button mPlusButton;
     private TextView mQuantityTextView;
+    private TextView mResultTextView;
+    private Button mOrderButton;
 
     // 수량
-    private int mQantity;
+    private int mQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +37,53 @@ public class MainActivity extends AppCompatActivity {
         // 레이아웃에서 특정 id를 인스턴스 변수와 연결
         mMinusButton = (Button) findViewById(R.id.minus_button);
         mPlusButton = (Button) findViewById(R.id.plus_button);
+        mOrderButton = (Button) findViewById(R.id.order_button);
         mQuantityTextView = (TextView) findViewById(R.id.quntity_text);
+        mResultTextView = (TextView) findViewById(R.id.result_text);
 
-        // 무명클래스
         mMinusButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                // debug
-                Log.d(TAG, "마이너스 버튼 클릭");
-                Log.v(TAG, "일반로그");
-                Log.e(TAG, "에러로그");
-                Log.i(TAG, "정보로그");
-                Log.w(TAG, "경고로그");
+                mQuantity--;
+                if (mQuantity < QUANTITY_MIN) {
+                    mQuantity = QUANTITY_MIN;
+                }
+                displayResult();
+            }
+        });
 
-                Toast.makeText(MainActivity.this, "메세지", Toast.LENGTH_SHORT).show();
+        // 무명클래스
+        mPlusButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mQuantity++;
+                if (mQuantity > QUANTITY_MAX) {
+                    mQuantity = QUANTITY_MAX;
+                }
+                displayResult();
+            }
+        });
+
+        mOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = mResultTextView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private void displayResult() {
+        mQuantityTextView.setText("" + mQuantity);
+
+        String result = "가격 : " + (COFFEE_PRICE * mQuantity)
+                + "원\n감사합니다";
+        mResultTextView.setText(result);
+    }
+
     private void init() {
-        mQantity = 0;
+        mQuantity = 0;
     }
 }
